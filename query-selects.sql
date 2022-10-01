c9dffb0e4be849cac9a7580df22bed6031a6ff57
danielumg22-269 <dnoriegac3@miumg.edu.gt>
2022-10-01 17:57:40 -0600

    Querys for selects tuples into the tables

---
+++ query-selects.sql
@@ -0,0 +1,7 @@
+SELECT *FROM [dbo].[measures];
+SELECT *FROM [dbo].[products];
+
+SELECT TOP (3) [product_id]
+      ,[product_code]
+      ,[product_description]
+ FROM [inventario].[dbo].[products];
