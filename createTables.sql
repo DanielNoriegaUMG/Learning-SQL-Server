6aa93b26f5aeb1150b0459beec35d98d23c74d17
danielumg22-269 <dnoriegac3@miumg.edu.gt>
2022-10-01 17:36:28 -0600

    Add query to create table orderDetails

--- createTables.sql
+++ createTables.sql
@@ -1,6 +1,6 @@
 USE inventario;
 
-CREATE TABLE [dbo].[Orders](
+CREATE TABLE [dbo].[orders](
 	order_id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	order_number INT NOT NULL,
 	order_date DATETIME NOT NULL,
@@ -8,7 +8,21 @@ CREATE TABLE [dbo].[Orders](
 );
 
 CREATE TABLE [dbo].[products](
-	product_id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	product_id INT NOT NULL	IDENTITY(1,1) PRIMARY KEY,
 	product_code VARCHAR(10) NOT NULL UNIQUE,
-	product_description VARCHAR(100) NOT NULL
-)
+	product_description	VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE [dbo].[orderDetails](
+	orderDetail_id	INT	NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	order_id INT NOT NULL,
+	orderDetail_number INT NOT NULL,
+	product_id INT NOT NULL,
+	orderDetail_quantity DECIMAL(18,2) NOT NULL,
+	orderDetail_price DECIMAL(18,2)	NOT NULL ,
+	measure_id INT NOT NULL,
+	notes VARCHAR(MAX) NULL,
+	CONSTRAINT orderDetails_orders_fk FOREIGN KEY (order_id) REFERENCES orders (order_id),
+	CONSTRAINT orderDetails_products_fk FOREIGN KEY (product_id) REFERENCES products (product_id),
+	CONSTRAINT orderDetails_measures_fk FOREIGN KEY (measure_id) REFERENCES measures (measure_id)
+);
