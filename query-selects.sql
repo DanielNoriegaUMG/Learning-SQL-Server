313c62cc2f95a85c5d8338bd8d7c3d0f90915ec4
danielumg22-269 <dnoriegac3@miumg.edu.gt>
2022-10-01 19:22:01 -0600

    Add querys with INNER JOIN between 3 tables

--- query-selects.sql
+++ query-selects.sql
@@ -1,7 +1,65 @@
 SELECT *FROM [dbo].[measures];
 SELECT *FROM [dbo].[products];
+SELECT *FROM [dbo].[orders];
+SELECT *FROM [dbo].[orderDetails];
 
+-----------------------------------------------
+-- TOP (limite de registros a traer)
 SELECT TOP (3) [product_id]
       ,[product_code]
       ,[product_description]
  FROM [inventario].[dbo].[products];
+
+SELECT TOP (1000) [orderDetail_id]
+      ,[order_id]
+      ,[orderDetail_number]
+      ,[product_id]
+      ,[orderDetail_quantity]
+      ,[orderDetail_price]
+      ,[measure_id]
+      ,[notes]
+FROM [inventario].[dbo].[orderDetails];
+
+-----------------------------------------------------
+-- Sentencia WHERE (seleccionar donde X registro sean iguales, mayores, diferentes, nulos, no nulos,etc)
+
+-- select con where para id mayor a 1
+SELECT *FROM [inventario].[dbo].[orders]
+WHERE order_id > 1;
+
+-- select para id igual a 1
+SELECT *FROM [inventario].[dbo].[orders]
+WHERE order_id = 1;
+
+--select para ver que descripciones de productos empiezan con letra L y siguen con cualquier letra
+SELECT *FROM [inventario].[dbo].[products]
+WHERE [product_description] LIKE 'L%';
+
+-- select para ver los precioes iguales o mayores a 15
+SELECT *FROM [inventario].[dbo].[orderDetails]
+WHERE [orderDetail_price] >= 15;
+
+-- select para ver las ordenedes donde tienen notas
+SELECT *FROM [inventario].[dbo].[orderDetails]
+WHERE [notes] IS NOT NULL;
+
+-- select para ver las ordenes donde no hay notas
+SELECT *FROM [inventario].[dbo].[orderDetails]
+WHERE [notes] IS NULL;
+
+-- select para las compras de la orden 1
+SELECT *FROM [inventario].[dbo].[orderDetails]
+WHERE [order_id] = 1;
+
+-- INNER JOIN (con alias)
+SELECT [ord].order_date, [det].*
+FROM [dbo].[orders] AS [ord]
+INNER JOIN [dbo].[orderDetails] AS [det]
+ON [ord].[order_id] = [det].order_id
+WHERE [ord].order_id = 1;
+
+SELECT [ord].order_date, [p].product_description, [det].[orderDetail_quantity],[det].[orderDetail_price]
+FROM [dbo].[orders] AS [ord]
+INNER JOIN [dbo].[orderDetails] AS [det] ON [ord].[order_id] = [det].[order_id]
+INNER JOIN [dbo].products AS [p] ON [det].product_id = [p].product_id
+WHERE [ord].[order_id] = 2;
