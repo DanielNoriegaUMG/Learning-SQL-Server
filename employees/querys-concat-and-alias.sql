0fd0146855e8b51b4f8672d8835c697733c4b88a
DanielNoriegaUMG <dnoriegac3@miumg.edu.gt>
2022-11-16 22:19:07 -0600

    learning function CAST to integer data

--- employees/querys-concat-and-alias.sql
+++ employees/querys-concat-and-alias.sql
@@ -1,7 +1,9 @@
 USE company;
 
 SELECT *FROM [dbo].[employees];
+SELECT*FROM  [dbo].[departaments];
 
+--function CONCAT
 SELECT e.employee_id,
 CONCAT(e.employee_name, ' ', e.employee_surname) AS 'employee_fullname',
 e.employee_birthday AS 'birthday',
@@ -11,4 +13,33 @@ CASE
 END AS 'genero',
 e.employee_salary
 FROM [dbo].[employees] AS [e]
+ORDER BY [e].[employee_salary] DESC;
+
+--function CAST
+SELECT e.employee_id,
+CONCAT(e.employee_name, ' ', e.employee_surname) AS 'employee_fullname',
+e.employee_birthday AS 'birthday',
+CASE 
+	WHEN e.employee_gender = 0 THEN 'H'
+	ELSE 'M'
+END AS 'genero',
+e.employee_salary,
+e.position_name + '/' + CAST(e.employee_salary AS nvarchar(100))
+FROM [dbo].[employees] AS [e]
+ORDER BY [e].[employee_salary] DESC;
+
+--NOTA: para evitar usar CAST es mas recomendable la funcion CONCAT() porque hace solo el paso de numeros a varchar
+
+SELECT e.employee_id,
+CONCAT(e.employee_name, ' ', e.employee_surname) AS 'employee_fullname',
+e.employee_birthday AS 'birthday',
+CASE 
+	WHEN e.employee_gender = 0 THEN 'H'
+	ELSE 'M'
+END AS 'genero',
+e.employee_salary,
+
+CONCAT(e.position_name,' / ',d.departament_name,' / ',e.employee_salary)
+FROM [dbo].[employees] AS [e]
+INNER JOIN [dbo].[departaments] AS [d] ON e.departament_id = d.departament_id
 ORDER BY [e].[employee_salary] DESC;
