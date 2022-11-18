3ded70a5668735acd5642d5bd9c04441d1db7727
DanielNoriegaUMG <dnoriegac3@miumg.edu.gt>
2022-11-17 21:25:18 -0600

    Add query to sentence DISTINCT

--- employees/top and distinct.sql
+++ employees/top and distinct.sql
@@ -3,3 +3,11 @@
 USE company;
 
 SELECT TOP(3) *FROM [dbo].[employees];
+
+SELECT TOP(10) *FROM [dbo].[employees] AS [e]
+ORDER BY [e].[created] ASC;
+
+SELECT *FROM [dbo].[employees];
+
+-- seleccionar todos los saraios y no mostrar mas de dos veces si encuentra un salario repetido
+SELECT DISTINCT [e].[employee_salary] FROM [dbo].[employees] AS [e];
