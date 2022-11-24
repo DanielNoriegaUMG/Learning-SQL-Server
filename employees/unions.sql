ff195a6dbbd1ab9ddab7c66a26e7254f995a3267
DanielNoriegaUMG <dnoriegac3@miumg.edu.gt>
2022-11-23 22:43:34 -0600

    Add querys to unions

--- employees/unions.sql
+++ employees/unions.sql
@@ -1,6 +1,6 @@
 -- UNION ALL		--> aparecen todas las filas repetidas
 -- UNION			--> devuelve filas distintas o unicas (no repetidas)
--- INTERSECT		--> devuelve filasen que ambas consultas son devueltas
+-- INTERSECT		--> devuelve filas en que ambas consultas son devueltas
 -- EXCEPT			--> devuelve filas encontradas en el primer query y adeas descartas las filas encontradas en el 2do o 3er query.
 
 USE company;
@@ -16,5 +16,25 @@ WHERE [e].[employee_salary] <= 5500.00;
 SELECT *, 'Query 1' AS 'Soruce' FROM [dbo].employees AS [e]
 WHERE [e].[employee_surname] LIKE '[^msa]%'
 UNION ALL
+SELECT *, 'Query 2' AS 'Soruce' FROM [dbo].employees AS [e]
+WHERE [e].[employee_salary] <= 5500.00;
+
+-- Misma query de arriba, pero eliminando repetidos, solo traera filas unicas
+SELECT *, 'Query 1' AS 'Soruce' FROM [dbo].employees AS [e]
+WHERE [e].[employee_surname] LIKE '[^msa]%'
+UNION
+SELECT *, 'Query 2' AS 'Soruce' FROM [dbo].employees AS [e]
+WHERE [e].[employee_salary] <= 5500.00;
+
+SELECT *FROM [dbo].employees AS [e]
+WHERE [e].[employee_name] LIKE 'N%'
+INTERSECT
+SELECT *FROM [dbo].employees AS [e]
+WHERE [e].[employee_salary] >= 8500.00;
+
+-- Ignora el resto de querys de ls consulta 2
+SELECT *, 'Query 1' AS 'Soruce' FROM [dbo].employees AS [e]
+WHERE [e].[employee_surname] LIKE '[^msa]%'
+EXCEPT
 SELECT *, 'Query 2' AS 'Soruce' FROM [dbo].employees AS [e]
 WHERE [e].[employee_salary] <= 5500.00;
