43b5af4643181860bff7eb4d27afbea2ef11494d
DanielNoriegaUMG <dnoriegac3@miumg.edu.gt>
2022-11-27 20:37:24 -0600

    add querys insert mails

---
+++ employees/insertsMails.sql
@@ -0,0 +1,27 @@
+USE company;
+SELECT *FROM [dbo].[employees];
+SELECT *FROM [dbo].[mails];
+INSERT INTO [dbo].[mails] ([mail],[employee_id])
+VALUES ('axelrom@gmail.com',1);
+
+INSERT INTO [dbo].[mails] ([mail],[employee_id])
+VALUES ('melSalazar@gmail.com',2),
+	('loren1980@gmail.com',3),
+	('tavoJuarez1995@gmail.com',4),
+	('mnicaGal@gmail.com',5),
+	('Arosas@gmail.com',6),
+	('chayitoRosa@outlook.com',7),
+	('nicoleOsorio@gmail.com',8),
+	('ferAlonso2000@yahoo.com',9),
+	('souf_orantes2002@gmail.com',10),
+	('sequisJuan@hotmail.es',11),
+	('ktrinaMol123@hotmail.es',12),
+	('SuperAmbrosio17@outlook.com',13),
+	('ramirogonza@outlook.com',14),
+	('villavicencio@outlook.com',15),
+	('delaRoca@outlook.com',16),
+	('BarbaraCami@yahoo.com',17),
+	('sepulveda3092@hotmail.com',18),
+	('aguirre9012@gmail.com',19),
+	('alexaMarroquin453@hotmail.es',20),
+	('dnoriegac3@outlook.com',22);
